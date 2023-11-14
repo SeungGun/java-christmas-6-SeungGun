ad730065dab54fc8784fc8c2b827ca6f56f2243e
SeungGun <saverrw2864@naver.com>
2023-11-14 16:15:28 +0900

    feat: 주문 정보의 총메뉴 개수 검증 로직 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -2,11 +2,30 @@ package christmas.domain;
 
 import java.util.List;
 
+import static christmas.ErrorMessage.*;
+import static christmas.ExceptionHandler.*;
+
 public class Order {
+    private static final int LIMIT_TOTAL_MENU_COUNT = 20;
+
     private final List<DetailOrder> detailOrders;
 
     public Order(List<DetailOrder> detailOrders) {
+        validateMenuCount(detailOrders);
         this.detailOrders = detailOrders;
+    }
+
+    private void validateMenuCount(List<DetailOrder> detailOrders) {
+        if (detailOrders.isEmpty()
+                || calculateTotalMenuCount(detailOrders) > LIMIT_TOTAL_MENU_COUNT) {
+            triggerArgException(INVALID_ORDER_MENU_COUNT);
+        }
+    }
+
+    private int calculateTotalMenuCount(List<DetailOrder> detailOrders) {
+        return detailOrders.stream()
+                .mapToInt(DetailOrder::getCount)
+                .sum();
     }
 }
 

b5af21662a6c05b7e1a2cbf363b18ea0555f1c42
SeungGun <saverrw2864@naver.com>
2023-11-15 16:35:39 +0900

    feat: 주문 메뉴목록 출력 UI 로직 구현

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -1,6 +1,9 @@
 package christmas.ui;
 
+import christmas.domain.DetailOrder;
 import christmas.util.ErrorMessage;
+
+import java.util.List;
 
 public class OutputView {
     public static void printErrorMessage(ErrorMessage message) {
@@ -14,5 +17,12 @@ public class OutputView {
     public static void printReadVisitDay() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
                 + "\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    public static void printOrderMenus(List<DetailOrder> detailOrders) {
+        System.out.println("<주문 메뉴>");
+        detailOrders.forEach(detailOrder
+                -> System.out.println(detailOrder.getMenu().getName()
+                + " " + detailOrder.getCount() + "개"));
     }
 }

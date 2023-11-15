f8862c07caccc19736d8c2a9ff16415c5afdbcdf
SeungGun <saverrw2864@naver.com>
2023-11-15 13:38:58 +0900

    test: 방문 날짜 입력 검증 테스트 케이스 추가

---
+++ src/test/java/christmas/InputValidatorTest.java
@@ -0,0 +1,18 @@
+package christmas;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class InputValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "32", "0", "100"})
+    void 유효하지_않은_방문_날짜_검증_예외테스트(String argument) {
+        Assertions.assertThatThrownBy(() -> InputValidator.validateVisitDayInput(argument))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_VISIT_DAY_INPUT.getMessage());
+    }
+
+}

f40556409e9629bfd7aa7224f221cc6105a6efcb
SeungGun <saverrw2864@naver.com>
2023-11-15 18:31:39 +0900

    fix: 에러 메세지 오탈자 수정

--- src/main/java/christmas/util/ErrorMessage.java
+++ src/main/java/christmas/util/ErrorMessage.java
@@ -1,13 +1,13 @@
 package christmas.util;
 
 public enum ErrorMessage {
-    INVALID_ORDER_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    NOT_EXIST_MENU_NAME_IN_MENU_BOARD("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    DUPLICATE_MENU_NAME_IN_ORDER("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    INVALID_EACH_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    CANNOT_ORDER_MENU_IS_ALL_BEVERAGE("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    INVALID_MENU_NAME_AND_COUNT_INPUT("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    INVALID_VISIT_DAY_INPUT("유효하지 않은 날짜입니다. 다시 입력해주세요.");
+    INVALID_ORDER_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    NOT_EXIST_MENU_NAME_IN_MENU_BOARD("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    DUPLICATE_MENU_NAME_IN_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_EACH_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    CANNOT_ORDER_MENU_IS_ALL_BEVERAGE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_MENU_NAME_AND_COUNT_INPUT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_VISIT_DAY_INPUT("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     private final String message;
 
     ErrorMessage(String message) {

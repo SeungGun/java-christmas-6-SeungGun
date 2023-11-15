9acef99f040b9d3fdd27a98a39ca9fb032bf1016
SeungGun <saverrw2864@naver.com>
2023-11-15 16:10:30 +0900

    feat: 주말 할인 이벤트 구현 클래스 정의 및 총 혜택 금액 계산 로직 구현

---
+++ src/main/java/christmas/domain/WeekendEvent.java
@@ -0,0 +1,22 @@
+package christmas.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
+public class WeekendEvent implements Event {
+    private static final Category BENEFIT_CATEGORY = Category.MAIN;
+    private final List<DayOfWeek> eventDayOfWeeks = List.of(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY);
+    private static final int BENEFIT_AMOUNT = 2023;
+
+    @Override
+    public int getBenefitAmount(Order order, LocalDate visitDate) {
+        if (!isEventApplicable(order)) {
+            return 0;
+        }
+        if (eventDayOfWeeks.contains(visitDate.getDayOfWeek())) {
+            return order.calculateMenuCountByCategory(BENEFIT_CATEGORY) * BENEFIT_AMOUNT;
+        }
+        return 0;
+    }
+}

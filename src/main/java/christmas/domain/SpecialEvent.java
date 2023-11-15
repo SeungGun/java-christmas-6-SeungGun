0f903f53a27c8c7b15dffb0a26305cb50967a211
SeungGun <saverrw2864@naver.com>
2023-11-15 16:08:39 +0900

    feat: 특별 할인 이벤트 구현 클래스 정의와 총 혜택 금액 계산 로직 구현

---
+++ src/main/java/christmas/domain/SpecialEvent.java
@@ -0,0 +1,20 @@
+package christmas.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class SpecialEvent implements Event {
+    private static final List<Integer> SPECIAL_DAYS = List.of(3, 10, 17, 24, 25, 31);
+    private static final int BENEFIT_AMOUNT = 1000;
+
+    @Override
+    public int getBenefitAmount(Order order, LocalDate visitDate) {
+        if(!isEventApplicable(order)){
+            return 0;
+        }
+        if (SPECIAL_DAYS.contains(visitDate.getDayOfMonth())) {
+            return BENEFIT_AMOUNT;
+        }
+        return 0;
+    }
+}

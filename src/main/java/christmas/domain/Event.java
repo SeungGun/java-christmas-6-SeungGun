cf38c5a23ec57ed898ffbd629fd5d93949ee633d
SeungGun <saverrw2864@naver.com>
2023-11-15 16:06:18 +0900

    feat: 이벤트 인터페이스 정의 및 총 혜택 금액 로직과 이벤트 적용 조건 검증 로직 구현

---
+++ src/main/java/christmas/domain/Event.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+import java.time.LocalDate;
+
+public interface Event {
+    int MIN_EVENT_APPLICABLE_AMOUNT = 10000;
+    int getBenefitAmount(Order order, LocalDate visitDate);
+
+    default boolean isEventApplicable(Order order){
+        return order.getTotalOrderAmount() >= MIN_EVENT_APPLICABLE_AMOUNT;
+    }
+}

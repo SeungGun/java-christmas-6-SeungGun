073f990be1c1f4ab1c553a71afc0da18f24abe9b
SeungGun <saverrw2864@naver.com>
2023-11-15 14:22:49 +0900

    fix: 주문 메뉴 입력을 위한 출력 안내에서 불필요한 내용 제거

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -6,8 +6,7 @@ public class OutputView {
     }
 
     public static void printReadOrderMenu() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n" +
-                "타파스-1,제로콜라-1");
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
     }
 
     public static void printReadVisitDay() {

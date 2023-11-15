a32ee6731f5ed31927b2c0220783d32adff10504
SeungGun <saverrw2864@naver.com>
2023-11-15 16:24:16 +0900

    fix: 증정 이벤트에서 증정 메뉴 조건 추가

--- src/main/java/christmas/domain/GiftEvent.java
+++ src/main/java/christmas/domain/GiftEvent.java
@@ -5,13 +5,18 @@ import java.time.LocalDate;
 public class GiftEvent implements Event {
     private static final String GIFT_MENU = "샴페인";
     private static final int GIFT_COUNT = 1;
+    private static final int MIN_GIFT_CONDITION = 120000;
 
     @Override
     public int getBenefitAmount(Order order, LocalDate visitDate) {
         if (!isEventApplicable(order)) {
             return 0;
         }
-        return getGiftMenuWithCount().calculateTotalAmount();
+
+        if (order.getTotalOrderAmount() >= MIN_GIFT_CONDITION) {
+            return getGiftMenuWithCount().calculateTotalAmount();
+        }
+        return 0;
     }
 
     public DetailOrder getGiftMenuWithCount() {

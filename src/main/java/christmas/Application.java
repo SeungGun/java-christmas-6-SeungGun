87fc9cd7d4bc611fc417f6dc977cd0b40e19b2ad
SeungGun <saverrw2864@naver.com>
2023-11-15 17:57:21 +0900

    feat: 이벤트 플래너 실행

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,9 @@
 package christmas;
 
+import christmas.service.EventPlanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new EventPlanner().show();
     }
 }

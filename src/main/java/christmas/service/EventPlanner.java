d30427dad499f1d48d14b2511a8ad376bd9c6305
SeungGun <saverrw2864@naver.com>
2023-11-15 17:45:53 +0900

    feat: 이벤트 플래너 프로세스 메소드 구현

--- src/main/java/christmas/service/EventPlanner.java
+++ src/main/java/christmas/service/EventPlanner.java
@@ -1,9 +1,14 @@
 package christmas.service;
 
 import christmas.domain.*;
+import christmas.ui.InputView;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+
+import static christmas.ui.OutputView.*;
+import static christmas.ui.OutputView.printEventBadge;
 
 public class EventPlanner {
     private static final int EVENT_YEAR = 2023;
@@ -16,5 +21,18 @@ public class EventPlanner {
         events.put(EventType.WEEKEND, new WeekendEvent());
         events.put(EventType.GIFT, new GiftEvent());
         events.put(EventType.SPECIAL, new SpecialEvent());
+    }
+
+    public void show() {
+        int visitDay = InputView.readVisitDay();
+        Order order = new Order(InputView.readOrders());
+
+        LocalDate visitDate = LocalDate.of(EVENT_YEAR, EVENT_MONTH, visitDay);
+
+        printEventTitleOnDate(visitDay);
+        printOrderMenus(order.getDetailOrders());
+        printTotalOrderAmountBeforeDiscount(order.getTotalOrderAmount());
+        printGiftMenu(((GiftEvent) events.get(EventType.GIFT)).getGiftMenuWithCount(order));
+        printAllBenefits(events, order, visitDate);
     }
 }

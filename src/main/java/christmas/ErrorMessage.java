eedeb20f176823f8316157f0f8524e59f828e465
SeungGun <saverrw2864@naver.com>
2023-11-15 13:37:46 +0900

    feat: 식당 방문 날짜 입력 검증 로직 구현

--- src/main/java/christmas/ErrorMessage.java
+++ src/main/java/christmas/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     DUPLICATE_MENU_NAME_IN_ORDER("유효하지 않은 주문입니다. 다시 입력해주세요."),
     INVALID_EACH_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해주세요."),
     CANNOT_ORDER_MENU_IS_ALL_BEVERAGE("유효하지 않은 주문입니다. 다시 입력해주세요."),
-    INVALID_MENU_NAME_AND_COUNT_INPUT("유효하지 않은 주문입니다. 다시 입력해주세요.");
+    INVALID_MENU_NAME_AND_COUNT_INPUT("유효하지 않은 주문입니다. 다시 입력해주세요."),
+    INVALID_VISIT_DAY_INPUT("유효하지 않은 날짜입니다. 다시 입력해주세요.");
     private final String message;
 
     ErrorMessage(String message) {
--- src/main/java/christmas/InputValidator.java
+++ src/main/java/christmas/InputValidator.java
@@ -1,13 +1,16 @@
 package christmas;
 
+import static christmas.ErrorMessage.*;
 import static christmas.ExceptionHandler.*;
 
 public class InputValidator {
     private static final int INPUT_MENU_WITH_COUNT_SPLIT_SIZE = 2;
+    private static final int MIN_VISIT_DAY = 1;
+    private static final int MAX_VISIT_DAY = 31;
 
     public static void validateMenuNameAndCountInput(String[] input) {
         if (input.length != INPUT_MENU_WITH_COUNT_SPLIT_SIZE) {
-            triggerArgException(ErrorMessage.INVALID_MENU_NAME_AND_COUNT_INPUT);
+            triggerArgException(INVALID_MENU_NAME_AND_COUNT_INPUT);
         }
     }
 
@@ -15,9 +18,22 @@ public class InputValidator {
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            triggerArgException(ErrorMessage.INVALID_MENU_NAME_AND_COUNT_INPUT);
+            triggerArgException(INVALID_MENU_NAME_AND_COUNT_INPUT);
         }
     }
 
+    public static void validateVisitDayInput(String input) {
+        try {
+            int parseDay = Integer.parseInt(input);
+            if (!isDayInRange(parseDay)) {
+                triggerArgException(INVALID_VISIT_DAY_INPUT);
+            }
+        } catch (NumberFormatException e) {
+            triggerArgException(INVALID_VISIT_DAY_INPUT);
+        }
+    }
 
+    private static boolean isDayInRange(int day) {
+        return day >= MIN_VISIT_DAY && day <= MAX_VISIT_DAY;
+    }
 }

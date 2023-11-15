6fd592592513986d0e1763b5c50cdeeb2a17cc9a
SeungGun <saverrw2864@naver.com>
2023-11-15 16:57:10 +0900

    feat: 이벤트 유형 Enum 클래스 정의

---
+++ src/main/java/christmas/domain/EventType.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+public enum EventType {
+    CHRISTMAS_D_DAY("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
+    GIFT("증정 이벤트");
+    private final String eventName;
+
+    EventType(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+}
